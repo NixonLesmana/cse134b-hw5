f4c541d2e5f9f0e534d0a19d90648fa037418bd9
NixonLesmana <nixonalexander0509@gmail.com>
2026-07-31 14:58:09 -0700

    Set up Eleventy build

---
+++ eleventy.config.js
@@ -0,0 +1,21 @@
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/media": "media" });
+
+  eleventyConfig.addFilter("startsWith", (value, prefix) => {
+    return String(value).startsWith(String(prefix));
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+}
